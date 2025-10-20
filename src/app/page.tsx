7dc4be7c726f69427614f7bd8b0d2cc759e6d164
ToobaTaqi <toobtq@gmail.com>
2025-10-20 23:55:00 +0500

    redeploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Trending from "./components/homePageComponents/Trending";
 import Popular from "./components/homePageComponents/Popular";
 
 export default function page() {
+  console.log("for redeploy")
   return (
     <div className="text-tertiary flex flex-col gap-6 lg:py-10">
       <Latest />
